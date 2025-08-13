dd6a476c54a646c71013d4252fe728bfbd280360
sakhareshrikant5 <sakhareshrikant5@gmail.com>
2025-08-13 13:10:43 +0400

    Added Notes component to App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AdviceTiles from './Components/AdviceTiles';
+import StudyNotes from './Components/Notes/StudyNotes';
 
 function App() {
 
   return (
     <div className="App">
      <AdviceTiles></AdviceTiles>
+     <StudyNotes></StudyNotes>
     </div>
   );
 } 
---
+++ src/Components/Notes/StudyNotes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function StudyNotes() {
+  return (
+    <div>
+      <p>My Notes</p>
+    </div>
+  )
+}
